fb5314f353efa8c1fa4b05798df8f7ca0081bc54
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-05-01 11:29:47 +0100

    fixed bug in init whitelist

--- programs/tier-sol-miner/src/contexts/init_whitelist.rs
+++ programs/tier-sol-miner/src/contexts/init_whitelist.rs
@@ -5,13 +5,13 @@ use crate::states::tier::TierInfo;
 use crate::states::user::UserInfo;
 use crate::states::whitelist::WhitelistInfo;
 use crate::errors::MinerError;
-use crate::math::{calculate_fee, calculate_interest};
+use crate::math::{calculate_fee, to_u128, calculate_interest};
 
 /// Instruction to call for whitelisted users that have not started staking SOL
 /// in a TIER. Initializes new user info account and accepts lamports to
 /// deposit into the vault. Whitelisted users does not need to hold tokens
 #[derive(Accounts)]
-#[instruction(_tier_name: &[u8])]
+#[instruction(_tier_nonce: u8)]
 pub struct InitWhiteList<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
@@ -24,30 +24,35 @@ pub struct InitWhiteList<'info> {
     )]
     pub user_info: Account<'info, UserInfo>,
     #[account(
-        seeds = [b"mine".as_ref(), mine_info.admin.as_ref()],
+        seeds = [b"mine"],
         bump = mine_info.bump,
-        constraint = mine_info.is_active @ MineError::InactiveMine
+        constraint = mine_info.is_active @ MinerError::InvalidMine
     )]
     pub mine_info: Account<'info, MineInfo>,
     #[account(
         mut,
-        seeds = [b"mine-vault", mine_info.admin.as_ref()],
+        seeds = [b"mine-vault"],
         bump = mine_vault.bump
     )]
     pub mine_vault: Account<'info, MineVault>,
     #[account(
-        seeds = [b"whitelist", signer.key().as_ref(), mine_info.admin.as_ref()],
+        mut,
+        seeds = [b"whitelist", signer.key().as_ref()],
         bump = whitelist_info.bump,
         close = signer
     )]
     pub whitelist_info: Account<'info, WhitelistInfo>,
     #[account(
         mut,
-        seeds: [tier_name.as_ref(), mine_info.admin.as_ref()],
-        bump = tier.bump,
-        constraint = tier.is_active @ MineError::InactiveTier
+        seeds = [b"tier", &[_tier_nonce]],
+        bump = tier_info.bump,
+        constraint = tier_info.is_active && whitelist_info.tier == tier_info.key() && _tier_nonce == tier_info.nonce @ MinerError::InvalidTier
     )]
     pub tier_info: Account<'info, TierInfo>,
+    #[account(
+        mut,
+        constraint = mine_info.fee_collector == fee_collector.key()
+    )]
     pub fee_collector: SystemAccount<'info>,
     pub system_program: Program<'info, System>
 }
@@ -57,18 +62,24 @@ impl<'info> InitWhiteList<'info> {
         &mut self,
         deposit_amount: u64,
         bump: u8,
-        _tier_name: &[u8]
+        _tier_nonce: u8
     ) -> Result<()> {
         if deposit_amount <= 0 {
-            err!(MinerError::InvalidDepositAmount);
+            return err!(MinerError::InvalidDepositAmount);
         }
         if (Clock::get()?.unix_timestamp as u64) > self.whitelist_info.expiry {
-            err!(MinerError::ExpiredWhiteList);
+            return err!(MinerError::ExpiredWhiteList);
         }
 
         // Calculate fees and transfer lamports to vault and fee collector
-        let dev_fee = calculate_fee(deposit_amount, self.mine_info.dev_fee);
-        let actual_amount = deposit_amount - dev_fee;
+        let dev_fee = match calculate_fee(
+            to_u128(deposit_amount)?, 
+            to_u128(self.mine_info.dev_fee)?
+        ) {
+            Ok(fee) => fee,
+            Err(_) => return err!(MinerError::MathsError)
+        };
+        let actual_amount = deposit_amount.saturating_sub(dev_fee);
         let fee_transfer_ix = system_instruction::transfer(
             self.signer.key,
             &self.mine_info.fee_collector,
@@ -99,11 +110,19 @@ impl<'info> InitWhiteList<'info> {
         )?;
 
         // Initialize User info
+        let interest_accrued = match calculate_interest(
+            to_u128(actual_amount)?, 
+            to_u128(self.tier_info.apy)?, 
+            to_u128(self.tier_info.lock_duration)?
+        ) {
+            Ok(interest) => interest,
+            Err(_) => return err!(MinerError::MathsError)
+        };
         self.user_info.set_inner(UserInfo {
             bump,
             owner: self.signer.key(),
             total_locked: actual_amount,
-            accrued_interest: calculate_interest(actual_amount, self.tier_info.apy, self.tier_info.lock_duration),
+            accrued_interest: interest_accrued,
             lock_ts: Clock::get()?.unix_timestamp as u64,
             tier: self.tier_info.key(),
             is_whitelist: true
